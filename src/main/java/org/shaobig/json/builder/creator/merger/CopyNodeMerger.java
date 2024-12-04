f076f2fc04cebefdc2f6d2da91cfa902af41125f
Ramon <shaobig@gmail.com>
2024-12-04 17:36:47 +0200

    JB-002: Substitute the forEach method to Splititerator

--- src/main/java/org/shaobig/json/builder/creator/merger/CopyNodeMerger.java
+++ src/main/java/org/shaobig/json/builder/creator/merger/CopyNodeMerger.java
@@ -3,12 +3,15 @@ package org.shaobig.json.builder.creator.merger;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.util.Spliterators;
+import java.util.stream.StreamSupport;
+
 public class CopyNodeMerger implements NodeMerger<ObjectNode> {
 
     @Override
     public ObjectNode mergeValue(ObjectNode parentNode, JsonNode childNode) {
-        childNode.fields().forEachRemaining(entry -> parentNode.set(entry.getKey(), entry.getValue()));
-        return parentNode;
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(childNode.fields(), 0), false)
+                .reduce(parentNode, (objectNode, entry) -> objectNode.set(entry.getKey(), entry.getValue()), (objectNode, objectNode2) -> objectNode);
     }
 
 }
